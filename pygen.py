e053fda6d5dcd820dea330905253ec41cd066e09
Egas1k <110778955+egrbt@users.noreply.github.com>
2025-09-21 20:29:20 +0300

    pygen 1.0.1
    Выполнен переход с sys.argv на библиотеку argparse.
    Теперь программа может работать с аргументами. На данный момент добавлено два аргумента -c/--count   - количество паролей на выводе и -d/--difficulty - сложность пароля. Эти аргументы опциональны, если их не задавать - программа выдаст пароль средней безопасности. Единственный обязательный аргумент - формат для генерации, пока он только один - password/

--- pygen.py
+++ pygen.py
@@ -1,23 +1,54 @@
-import sys
+import argparse
 import random
 
-first_arg = sys.argv[1]
-#second_arg = sys.argv[2]
+
+parser = argparse.ArgumentParser(description='Password generator software(pygen 1.0.1)', usage='pygen [password] [-d <difficulty level>] [-c <count>]')
+
+parser.add_argument('mode',type=str,help='for generate password(s): password / for generate wordlist: wordlist')
+parser.add_argument('-c','--count',type=int,help='count of passwords')
+parser.add_argument('-d','--difficulty',type=str,help='easy/medium/hard')
+args = parser.parse_args()
+
+if args.difficulty:
+    difficulty = args.difficulty
+else:
+    difficulty = 'medium'
+
+if args.count:
+    count = args.count
+else:
+    count = 1
+
+list_all_symbols = [0,1,2,3,4,5,6,7,8,9,'[',']','{','}',';',':','"',"'",'<','>','?','/','|','~','!','@','#','$','%','^','&','*','(',')','-','+','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-list_all_symbols = [0,1,2,3,4,5,6,7,8,9,'~','!','@','#','$','%','^','&','*','(',')','-','+','q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l']
+match args.mode:
+    case 'password':
+        if difficulty == 'hard':
+            for i in range(count):
+                password = []
+                for i in range(12):
+                    symbol_to_append = random.choice(list_all_symbols)
+                    password.append(symbol_to_append)
+                print(*password[ :12 ], sep='')
 
-print(first_arg)                            #Вывод введённых аргументов для откладки
+
+        if difficulty == 'medium':
+                for i in range(count):
+                    password = []
+                    for i in range(8):
+                        symbol_to_append = random.choice(list_all_symbols)
+                        password.append(symbol_to_append)
+                    print(*password[ :8 ], sep='')
 
 
-match first_arg:
-    case '-h':
-        print("Welcome to pygen 1.0.0!\nUsage: pygen -P")
-    case '-p':
-        password = []
-        for i in range(8):
-                symbol_to_append = random.choice(list_all_symbols)
-                password.append(symbol_to_append)
-        print(*password[ :8 ], sep='')
+        if difficulty == 'easy':
+                for i in range(count):
+                    password = []
+                    for i in range(5):
+                        symbol_to_append = random.choice(list_all_symbols)
+                        password.append(symbol_to_append)
+                    print(*password[ :5 ], sep='')
+                    
     case _:
         print('Unknown command :(')
